13116cd0a53a58c90a8538438f885d563bc10ebb
unknown <eshwarkodavali123@gmail.com>
2025-09-22 22:37:27 +0530

    condition3

---
+++ condition_3.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+if [ $(id -u) -eq 0 ]; then
+    echo "it is a root access you can proceed"
+else
+    echo "Need to run with root access"
+    exit 1
+fi
+VALIDATE(){
+    if [ $? -eq 0 ]; then
+        echo "Installation for $1 is success"
+    else
+        echo "Installation for $1 is failed"
+fi
+}
+dnf install mysql -y
+VALIDATE "mysql"
+
+dnf install ngnix -y
+VALIDATE "nginix"
+
+dnf install python3 -y
+VALIDATE "python"
+
