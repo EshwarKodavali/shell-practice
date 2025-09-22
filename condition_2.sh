43636ce223356b46f3389d6dd487aa675483fd3d
unknown <eshwarkodavali123@gmail.com>
2025-09-22 22:00:24 +0530

    condition2

---
+++ condition_2.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+if [ $(id -u) -eq 0 ]; then
+    echo "it is a root access you can proceed"
+else
+    echo "Need to run with root access"
+    exit
+
+dnf install mysql -y
+
+if [ $($?) -eq 0 ]; then
+    echo "Installation is success"
+else
+    echo "Installation is failed"
