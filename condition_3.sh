01055b5058d4e5361b70fc83bfed3a32ddbd6c5b
unknown <eshwarkodavali123@gmail.com>
2025-09-22 23:09:47 +0530

    condition

--- condition_3.sh
+++ condition_3.sh
@@ -6,19 +6,37 @@ else
     echo "Need to run with root access"
     exit 1
 fi
+
 VALIDATE(){
     if [ $? -eq 0 ]; then
         echo "Installation for $1 is success"
     else
         echo "Installation for $1 is failed"
-fi
+    fi
 }
-dnf install mysql -y
-VALIDATE "mysql"
 
-dnf install nginx -y
-VALIDATE "nginx"
+dnf list installed mysql
+if [ $? -nq 0 ]; then
+    dnf install mysql -y
+    VALIDATE "mysql"
+else
+    echo "Installation is already done"
+fi
 
-dnf install python3 -y
-VALIDATE "python"
+dnf list installed nginx
+if [ $? -nq 0 ]; then
+    dnf install nginx -y
+    VALIDATE "nginx"
+else
+    echo "Installation is already done"
+fi
+
+dnf list installed python3
+if [ $? -nq 0 ]; then
+    dnf install python3 -y
+    VALIDATE "python3"
+else
+    echo "Installation is already done"
+fi
+
 
