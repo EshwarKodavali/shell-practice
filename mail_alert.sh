bbcc76cfb77f22f6be5b340781e4fbda0c2fb5ee
unknown <eshwarkodavali123@gmail.com>
2025-09-29 18:07:52 +0530

    changed in zip code

--- mail_alert.sh
+++ mail_alert.sh
@@ -2,12 +2,15 @@
 
 DISK_USAGE=$(df -hT | grep -v Filesystem)
 DISK_THRESHOLD=2
+MESSAGE=""
+IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        echo "Alert: Hi usage on $PARTITION WITH VALUE: $USAGE"
+        MESSAGE+="Alert: High usage on $PARTITION WITH VALUE: $USAGE \n"
     fi
 
 done <<< $DISK_USAGE
+echo -e "Message body: $MESSAGE
