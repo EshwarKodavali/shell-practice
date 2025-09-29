515bc60b15c4a449db450bebb889eb3835eaf7e8
unknown <eshwarkodavali123@gmail.com>
2025-09-29 17:13:56 +0530

    changed in zip code

--- mail_alert.sh
+++ mail_alert.sh
@@ -6,8 +6,8 @@ while IFS= read -r line
 do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
-    if [ $USAGE -gt $DISK_THRESHOLD ]; then
-        echo "Alert on $PARTIOIN WITH VALUE: $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]; then
+        echo "Alert: Hi usage on $PARTIOIN WITH VALUE: $USAGE"
     fi
 
 done <<< $DISK_USAGE
